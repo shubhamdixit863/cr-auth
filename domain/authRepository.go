23d473b684992e0c80f893ff7c781dd986c9c626
shubham <shubhamdixit863@gmail.com>
2022-10-28 22:07:46 +0530

    Modified Signup api added error

--- domain/authRepository.go
+++ domain/authRepository.go
@@ -20,8 +20,7 @@ type AuthRepositoryDb struct {
 
 func (d AuthRepositoryDb) AddUser(user *User) *errs.AppError {
 
-	_, err := d.client.NamedExec(`INSERT INTO users (username,password, email,name)
-        VALUES (:username, :password, :email,:name)`, user)
+	_, err := d.client.NamedExec(`INSERT INTO users (username,password, email,name) VALUES (:username, :password, :email,:name)`, user)
 	if err != nil {
 		return errs.NewUnexpectedError("Problem In Inserting In The Db" + err.Error())
 	}
