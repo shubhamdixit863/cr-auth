fbb5cd5a61ee64ba56705df41361c000d69f9d19
shubham <shubhamdixit863@gmail.com>
2022-10-30 15:09:54 +0530

    Added

--- migration/up.sql
+++ migration/up.sql
@@ -7,3 +7,10 @@ CREATE TABLE `banking`.`users` (
                                    PRIMARY KEY (`id`),
                                    UNIQUE INDEX `email_UNIQUE` (`email` ASC),
                                    UNIQUE INDEX `username_UNIQUE` (`username` ASC));
+
+
+
+CREATE TABLE `banking`.`refresh_token_store` (
+                                                 `id` INT NOT NULL AUTO_INCREMENT,
+                                                 `refresh_token` VARCHAR(245) NULL,
+                                                 PRIMARY KEY (`id`));
